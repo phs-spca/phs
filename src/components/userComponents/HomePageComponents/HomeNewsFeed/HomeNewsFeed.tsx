172e7dd190dd2e5c600c5989c5797890b35ec692
Peninsula Humane Society & SPCA <phs.spca.retool@gmail.com>
2025-03-03 16:41:43 -0800

    Updated HomeNewsFeed.tsx again
    Trying to see if I can figure out how to represent the css styling as an object for react, this approach right now probably won't work though. But, might as well try anyway!

--- src/components/userComponents/HomePageComponents/HomeNewsFeed/HomeNewsFeed.tsx
+++ src/components/userComponents/HomePageComponents/HomeNewsFeed/HomeNewsFeed.tsx
@@ -31,11 +31,17 @@ function HomeNewsFeed() {
      justify-center items-center"
     >
       {isWebDevice && (
-        <iframe title="Instagram Feed" id="instagram-embed-0" 
-          src="https://www.instagram.com/phsspcawildlife/embed/?cr=1&amp;v=14&amp;wp=540&amp;rd=file%3A%2F%2F&amp;rp=%2FC%3A%2FUsers%2Fedidato%2FDownloads%2Ftesting_embed%2520-%2520Copy.html#%7B%22ci%22%3A0%2C%22os%22%3A762.3999999994412%2C%22ls%22%3A60.39999999944121%2C%22le%22%3A758.3999999994412%7D" 
-          allowfullscreen="true" height="583" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" 
-          style="background: white; max-width: 540px; width: calc(100% - 2px); border-radius: 3px; border: 1px solid rgb(219, 219, 219); box-shadow: none; display: block; margin: 0px 0px 12px; min-width: 326px; padding: 0px;">
-        </iframe>
+        <iframe title="Instagram Feed" class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/phsspcawildlife/embed/?cr=1&amp;v=14&amp;wp=540&amp;rd=file%3A%2F%2F&amp;rp=%2FC%3A%2FUsers%2Fedidato%2FDownloads%2Ftesting_embed%2520-%2520Copy.html#%7B%22ci%22%3A0%2C%22os%22%3A23.899999999441206%2C%22ls%22%3A13.099999999627471%2C%22le%22%3A15.399999999441206%7D" allowfullscreen="true" frameborder="0" height="583" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style={{
+    background: white 
+    max-width: 540px
+    width: calc(100% - 2px) 
+    border-radius: 3px 
+    border: 1px solid rgb(219, 219, 219) 
+    box-shadow: none 
+    display: block
+    margin: 0px 0px 12px
+    min-width: 326px
+    padding: 0px}}></iframe>
       )}
     </div>
   );
