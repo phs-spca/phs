f66c00c909ae3a20fdc0c4a1047ffd1e3bdd0934
Peninsula Humane Society & SPCA <phs.spca.retool@gmail.com>
2025-03-07 13:52:41 -0800

    Update SiteMap.tsx change zoom to 18

--- src/components/userComponents/SiteMap/SiteMap.tsx
+++ src/components/userComponents/SiteMap/SiteMap.tsx
@@ -148,7 +148,7 @@ function SiteMap({ mode }: SiteMapProps) {
   return (
     <MapContainer
       center={mapCenter}
-      zoom={24}
+      zoom={18}
       zoomControl={false}
       scrollWheelZoom
       style={{
