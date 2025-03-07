e3fc544fdc807634c59b17c92a90dcc37373a450
Peninsula Humane Society & SPCA <phs.spca.retool@gmail.com>
2025-03-07 09:07:13 -0800

    Update queries.tsx another attempt at sorting spotlights

--- src/supabase/tours/queries.tsx
+++ src/supabase/tours/queries.tsx
@@ -23,7 +23,7 @@ export async function fetchAllSpotlights(): Promise<TourRow[]> {
   const { data, error } = await supabase
     .from('tours')
     .select('*')
-    .eq('category', sitefeatures)
+    .eq('category', spotlight)
   if (error) {
     throw new Error(
       `An error occurred while trying to read tour displays: ${error}`,
