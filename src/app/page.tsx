a64bcbf4a664fb06a3d0dba6bd450e5ef5ecaff7
Peninsula Humane Society & SPCA <phs.spca.retool@gmail.com>
2025-03-10 15:12:01 -0700

    Attempting to fix welcome graphic on page.tsx
    It appears someone tried to just put the html for displaying the welcome graphic after the element has already been made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,17 +29,6 @@ function Home() {
     <div className="HomePage w-full h-full m-0 flex-col justify-center font-normal">
       <NavBar />
       <WelcomeGraphic />
-            <div className="IntroInfo web:w-full web:h-72 bg-ivory web:bg-mint-cream inline-flex flex-col justify-center items-center">
-        {!isWebDevice && (
-          <p className="b3 w-full px-4 py-10 text-night text-center text-base gap-4">
-            The Peninsula Humane Society & SPCA (PHS/SPCA) is a local, private,
-            non-profit charitable organization dedicated to animal welfare.
-            PHS/SPCA is truly an open admission shelter, not only accepting many
-            pet animals who might be refused at other shelters, but also taking
-            care of sick, injured, and orphaned native wildlife.
-          </p>
-        )}
-      </div>
       <HomeWildlifeSpotlights />
       <VisitorResources />
       <HomeVirtualTours />
